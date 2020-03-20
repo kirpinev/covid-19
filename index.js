2f1f31ca72a99b5c9e654b1164e1052d76dd9061
Igor Kirpinev <ikirpinev@gmail.com>
2020-03-20 16:00:09 +0300

    Добавил таблицу для отображения топ 10 стран

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Api from "./modules/api.js";
 import Lead from "./modules/lead.js";
 import Header from "./modules/header.js";
+import Table from "./modules/table.js";
 
 const api = new Api({
   url: "https://covid19.mathdro.id/api"
@@ -10,5 +11,10 @@ const lead = new Lead(document.querySelector(".total__number"), api);
 
 const header = new Header(document.querySelector(".header__total"), api);
 
+const table = new Table(document.querySelector(".table"), api);
+
 lead.renderConfirmed("/confirmed");
+
 header.renderConfirmed("/confirmed");
+
+table.render("/confirmed", 10);
